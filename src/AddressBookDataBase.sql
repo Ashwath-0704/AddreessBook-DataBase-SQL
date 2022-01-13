8757a4bb75edf5913ac4e07a92e0082743344c68
Achyuth Naidu <ashwath.bly@gamil.com>
2022-01-13 16:36:16 +0530

    Added : Ability to create a Address Book Service DB

---
+++ src/AddressBookDataBase.sql
@@ -0,0 +1,23 @@
+
+// UC1 :- Ability to create a Address Book Service DB
+
+mysql> create database Address_Book_DataBase;
+Query OK, 1 row affected (0.03 sec)
+
+mysql>  use Address_Book_DataBase;
+Database changed
+mysql> show databases;
++-----------------------+
+| Database              |
++-----------------------+
+| address_book_database |
+| demo                  |
+| information_schema    |
+| mysql                 |
+| payrool_service       |
+| performance_schema    |
+| sakila                |
+| sys                   |
+| world                 |
++-----------------------+
+9 rows in set (0.04 sec)
